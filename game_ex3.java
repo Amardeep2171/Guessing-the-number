b1c452f7f48028326e113c4375da0d4dbc16ef50
Amardeep <amardeepkramar@gmail.com>
2025-04-27 13:38:20 +0530

    Add files via upload
    The Number Guessing Game is a simple yet fun console-based game developed in Java. In this game, the computer randomly selects a number within a specified range (for example, 1 to 100), and the player has to guess it. After each guess, the game gives feedback,  telling the player if the guess was too high, too low, or correct. The game continues until the player guesses the right number.
    
    Features:
    
    Random number generation using Java’s Random class.
    
    User input handled via Scanner.
    
    Feedback after each guess to guide the player.
    
    Keeps track of the number of attempts.
    
    Option to play again after winning

---
+++ game_ex3.java
@@ -0,0 +1,52 @@
+ import java.util.Random;
+import java.util.Scanner;
+import java.util.random.*;
+ class Game{
+    public int number;
+    public int inputNumber;
+    public int noOfGuesses;
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+    public void setNoOfGuesses(int noOfGuesses){
+        this.noOfGuesses = noOfGuesses;
+    }
+
+    Game(){
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
+    }
+    void takeUserInput(){
+        System.out.println("guess the number");
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
+
+    }
+    boolean IsCorrectNumber(){
+        noOfGuesses++;
+        if(inputNumber == number){
+            System.out.format("yes you guessed it right number, it was %d \n You guessed it in %d attempts" , +number, +noOfGuesses);
+            return true;
+        } 
+        else if(inputNumber < number){
+            System.out.println("Too less");
+        }
+        else if(inputNumber > number){
+            System.out.println("Too high");
+        }
+        return false;
+
+    }
+ } 
+public class game_ex3{
+    public static void main(String[] args) {
+        boolean b = false;
+        while (!b) {
+            
+        Game g = new Game();
+        g.takeUserInput ();
+        b = g.IsCorrectNumber();
+       
+        }
+    }
+}
